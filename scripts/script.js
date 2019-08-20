d6fc0491e10bc4bd214b8849526447f86d75762b
Armenia Cole <armenia.cole@hotmail.co.nz>
2019-08-20 15:52:12 -0400

    Basic html and database added

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+const randomKiwi = {};
+
+randomKiwi.data = [
+    {
+        word: `Togs`,
+        definition: `You wear them swimming! Togs is the collective term for all forms of swim-wear (board shorts, bikinis, one-piece, speedos ect).`
+    },
+    {
+        word: `Jandals`,
+        definition: `Flip-flops! the origin of this word is "Japanese Sandals`
+    },
+    {
+        word: `Dairy`,
+        definition: `Convenience Store! Usually a local corner store, will sell essentials and treats.`
+    },
+    {
+        word: `Wop Wops`,
+        definition: `The middle of no-where! The wop wops is far away from or not surrounded by human activity. Could mean both rural or wild areas`
+    },
+    {
+        word: `Kiwi`,
+        definition: `NOT THE FRUIT! 1) A Kiwi refers to the flightless bird that is the national symbol of New Zealand. 2) Someone from New Zealand is also known as a Kiwi. For the fruit please see 'Kiwi-fruit'.`
+    },
+    {
+        word: `Bach`,
+        definition: `A little house for vacations. Similar to a Canadian "Cottage", a Bach is a small property usually near the beach to enjoy in summer.`
+    },
+    {
+        word: `Tramping`,
+        definition: `Hiking! Going tramping is a popular activity in New Zealand.`
+    },
+    {
+        word: `Yarn`,
+        definition: `Telling a story! Chatting with someone, specifically the act of telling a story would be considered "Having a yarn".`
+    },
+    {
+        word: `Cuz/ Cuzzie`,
+        definition: `Cuz or Cuzzie is short for Cousin but can mean anyone close to you that is like family.`
+    },
+    {
+        word: `Munted`,
+        definition: `Broken! if something is "Munted" it could also be described as broken, ruined, damaged, or f**ked up.`
+    }
+]
