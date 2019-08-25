3621b96e4bdb209edd7d502d8fe115ba4746a173
Armenia Cole <armenia.cole@hotmail.co.nz>
2019-08-25 14:23:31 -0400

    confused

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // const randomKiwi = {};
 
 // array of objects with words and definitions as keys.
@@ -98,6 +99,146 @@
 //         q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">To take a nap.</label>`
 //     },
 // ]
+=======
+// import randomKiwi from "./dataBase.js";
+
+const randomKiwi = {};
+
+// array of objects with words and definitions as keys.
+
+randomKiwi.dataBase = [
+    {
+        word: `Togs`,
+        definition: `<h3>You wear them swimming!</h3><p>Togs is the collective term for all forms of swim-wear (board shorts, bikinis, one-piece, speedos ect).</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="true"><label for="q1">Clothes for swimming.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">A pair of slippers.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">A local shellfish.</label>`
+
+    },
+    {
+        word: `Jandals`,
+        definition: `<h3>Flip-flops!</h3><p>The origin of this word is "Japanese Sandals"</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">"Goodbye!"</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="true"><label for="q2">Flip-flops.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">A type of candy.</label>`
+    },
+    {
+        word: `Dairy`,
+        definition: `<h3>Convenience Store!</h3><p>Usually a local corner store, will sell essentials and treats.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">A group of cows.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">An ice-cream.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="true"><label for="q3">A convenience store.</label>`
+    },
+    {
+        word: `Wop Wops`,
+        definition: `<h3>The middle of no-where!</h3><p>The wop wops is far away from or not surrounded by human activity. Could mean both rural or wild areas</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="true"><label for="q1">The middle of nowhere.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">When you discipline a child.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">A type of candy.</label>`
+
+    },
+    {
+        word: `Kiwi`,
+        definition: `<h3>NOT THE FRUIT!</h3><p>1) A Kiwi refers to the flightless bird that is the national symbol of New Zealand.</p><p>2) Someone from New Zealand is also known as a Kiwi. For the fruit please see 'Kiwi-fruit'.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">The bird, the fruit and a New Zealander.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">The fruit and the bird.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="true"><label for="q3">A bird and a New Zealander.</label>`
+    },
+    {
+        word: `Bach`,
+        definition: `<h3>A little house for vacations!</h3><p>Similar to a Canadian "Cottage", a Bach(batch) is a small property usually near the beach to enjoy in summer.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">A bachelor party.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="true"><label for="q2">A small vacation home.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">Someone who is obsessed with music.</label>`
+    },
+    {
+        word: `Tramping`,
+        definition: `<h3>Hiking!</h3><p>Going tramping is a popular activity in New Zealand.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">To have a party in the woods.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="true"><label for="q2">To walk in a non urban area.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">To be living rough.</label>`
+    },
+    {
+        word: `Yarn`,
+        definition: `<h3>Telling a story!</h3><p>Chatting with someone, specifically the act of telling a story would be considered "Having a yarn".</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">A boring person.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">To heard lambs.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="true"><label for="q3">To tell a long winded story.</label>`
+    },
+    {
+        word: `Cuz / Cuzzie`,
+        definition: `<h3>Cousin / Close Friend</h3><p>Cuz or Cuzzie is short for Cousin but can mean anyone close to you that is like family.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">Your cousin.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">A close friend.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="true"><label for="q3">All of the above.</label>`
+
+    },
+    {
+        word: `Munted`,
+        definition: `<h3>Broken!</h3><p>If something is "Munted" it could also be described as broken, ruined, damaged, or fucked up.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="true"><label for="q1">Something that is broken.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">To be confused.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">To be drunk.</label>`
+    },
+    {
+        word: `Stubbie`,
+        definition: `<h3>Small bottle of beer!</h3><p>A stubbie is the nickname for a 330ml bottle of beer.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">A short pair of pants.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">To be short.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="true"><label for="q3">Bottle of beer.</label>`
+    },
+    {
+        word: `Stubbies`,
+        definition: `<h3>Short shorts!</h3><p>Stubbies are a pair of very short shorts originally for men. They are similar to a rugby style short and are typically of a sturdy material.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">Ticket stub collectors.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="true"><label for="q2">Very short shorts.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">Toes.</label>`
+    },
+    {
+        word: `Slash`,
+        definition: `<h3>To go pee!</h3><p>Slang word used to excuse yourself to the bathroom in a party setting. "I'm going to take a slash".</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="true"><label for="q1">Going to the bathroom.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">To cut school.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">To take a nap.</label>`
+    },
+    {
+        word: `Squizz`,
+        definition: `<h3>Have a look!</h3><p>Commonly used in sentences like "I went to take a squizz." and "Give me a squizz.".</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">Go to the bathroom.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">To grab a snack.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="true"><label for="q3">To look at.</label>`
+    },
+    {
+        word: `Ute`,
+        definition: `<h3>Pick up truck!</h3><p>Trucks typically used for farming or off-roading.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="true"><label for="q1">Pick up truck.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">A large shovel.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">The "youths".</label>`
+    },
+    {
+        word: `Boot`,
+        definition: `<h3>Car Trunk!</h3><p>The storage section in a car.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">A pint of beer.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="true"><label for="q2">Back of a vehicle.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">Kicked out.</label>`
+    },
+    {
+        word: `Chilly-bin`,
+        definition: `<h3>Cooler!</h3><p>A box used to keep food and drink cool.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">A deep freezer.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">A box of beers.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="true"><label for="q3">A cold storage box.</label>`
+    },
+    {
+        word: `Tiki-tour`,
+        definition: `<h3>one liner!</h3><p>definition.</p>`,
+        q1: `<input type="radio" name="quest" id="q1" value="false"><label for="q1">one.</label>`,
+        q2: `<input type="radio" name="quest" id="q2" value="false"><label for="q2">two.</label>`,
+        q3: `<input type="radio" name="quest" id="q3" value="false"><label for="q3">three.</label>`
+    },
+
+]
+>>>>>>> Stashed changes
 
 
 // Function for generating a random number 
@@ -113,7 +254,7 @@ let userClick = '';
 randomKiwi.buttonClick = function(){
     $('.mainButton').on('submit', function(event) {
         event.preventDefault();
- 
+        console.log(randomKiwi);
         // generate a random number and use get a index of the array
         userClick = randomKiwi.randomItemFromData(randomKiwi.dataBase);
 
